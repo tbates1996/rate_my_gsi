848562b4e77ec3b32d878b188acf1352587bfd54
Thomas Bates <tbates@ymail.com>
2017-11-21 02:32:37 -0800

    Added profile page

---
+++ db/migrate/20171121091212_add_user_information.rb
@@ -0,0 +1,7 @@
+class AddUserInformation < ActiveRecord::Migration[5.1]
+  def change
+	add_column :users, :first_name, :string
+	add_column :users, :last_name, :string
+	add_column :users, :school, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171115193451) do
+ActiveRecord::Schema.define(version: 20171121091212) do
 
   create_table "courses", force: :cascade do |t|
     t.string "subject"
@@ -51,6 +51,9 @@ ActiveRecord::Schema.define(version: 20171115193451) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "school"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
