3e62350aceb849655cf7db3ed4f8e7e8887c2ce9
Kevin Ponek <kponek@berkeley.edu>
2017-11-27 04:01:53 -0800

    made subject, course_number, and school fields mandatory

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,9 @@
 class Course < ApplicationRecord
 	has_many :gsis, dependent: :destroy
 	has_many :reviews, through: :gsis
+
+	validates :school, presence: true
+	validates :course_number, presence: true
+	validates :subject, presence: true
 end
+
